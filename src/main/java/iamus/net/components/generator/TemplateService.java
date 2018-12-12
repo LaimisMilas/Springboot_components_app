363ebc85e3294cf982c0f1e14409d614f1f9eaf2
Inspiron <tuckuslukas@gmail.com>
2018-12-12 15:28:32 +0200

    .

--- src/main/java/iamus/net/components/generator/TemplateService.java
+++ src/main/java/iamus/net/components/generator/TemplateService.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import iamus.net.components.component.Component;
-import iamus.net.components.property.Property;
 
 
 
